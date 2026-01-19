4f7504f13700659eb20a0ddd067f55a437047003
Roosta078 <tommyagnello78@gmail.com>
2026-01-18 22:09:40 -0500

    implement applet functionality.  Still needs work

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,43 @@ use crossterm::event::{self, KeyCode, KeyEvent};
 use ratatui::DefaultTerminal;
 use ratatui::layout::Constraint;
 use ratatui::style::{Style, Stylize};
+use ratatui::text::Text;
 use ratatui::widgets::{Block, List, ListDirection, ListItem, ListState, Row, Table, TableState};
 mod db;
 
 struct App {
     state: AppState,
-    top_menu_options: Vec<String>,
+    applets: Vec<Box<dyn Applet>>,
     list_state: ListState,
 }
 
 pub trait Applet {
-    fn run(&mut self, terminal: &mut DefaultTerminal) -> Result<(), Box<dyn std::error::Error>>;
+    fn run(&mut self, terminal: &mut DefaultTerminal) -> Result<(), Box<dyn std::error::Error>> {
+        let text = Text::raw("TODO");
+        terminal.draw(|frame| frame.render_widget(text, frame.area()))?;
+        Ok(())
+    }
     fn get_name(&self) -> String;
+}
+
+struct ListLocationApplet {}
+struct ListItemsApplet {}
+
+impl Applet for ListLocationApplet {
+    // fn run(&mut self, _terminal: &mut DefaultTerminal) -> Result<(), Box<dyn std::error::Error>> {
+    //     Ok(())
+    // }
+    fn get_name(&self) -> String {
+        "List Locations".to_string()
+    }
+}
+impl Applet for ListItemsApplet {
+    // fn run(&mut self, _terminal: &mut DefaultTerminal) -> Result<(), Box<dyn std::error::Error>> {
+    //     Ok(())
+    // }
+    fn get_name(&self) -> String {
+        "List Items".to_string()
+    }
 }
 
 enum AppState {
@@ -26,11 +51,8 @@ enum AppState {
 impl Default for App {
     fn default() -> Self {
         let mut app = Self::new();
-        app.top_menu_options = vec![
-            "List Locations".to_string(),
-            "Item 2".to_string(),
-            "Quit".to_string(),
-        ];
+        app.applets.push(Box::new(ListLocationApplet {}));
+        app.applets.push(Box::new(ListItemsApplet {}));
         app
     }
 }
@@ -39,7 +61,7 @@ impl App {
     fn new() -> Self {
         Self {
             state: AppState::TopMenu,
-            top_menu_options: Vec::new(),
+            applets: Vec::new(),
             list_state: ListState::default(),
         }
     }
@@ -49,7 +71,8 @@ impl App {
             match self.state {
                 AppState::Exit => break,
                 AppState::TopMenu => self.run_top_menu(terminal)?,
-                AppState::ListLocations => self.run_list_locations(terminal)?,
+                AppState::ListLocations => self.applets[0].run(terminal)?,
+                AppState::ListItems => self.applets[1].run(terminal)?,
                 _ => break,
             }
         }
@@ -60,17 +83,22 @@ impl App {
         &mut self,
         terminal: &mut DefaultTerminal,
     ) -> Result<(), Box<dyn std::error::Error>> {
-        let list = List::new(self.top_menu_options.clone())
-            .block(
-                Block::bordered()
-                    .title("Inventory Manager")
-                    .title_bottom("Press 'q' or Esc to exit"),
-            )
-            .style(Style::new().white())
-            .highlight_style(Style::new().bold())
-            .highlight_symbol(">>")
-            .repeat_highlight_symbol(true)
-            .direction(ListDirection::TopToBottom);
+        let list = List::new(
+            self.applets
+                .iter()
+                .map(|a| a.get_name())
+                .collect::<Vec<String>>(),
+        )
+        .block(
+            Block::bordered()
+                .title("Inventory Manager")
+                .title_bottom("Press 'q' or Esc to exit"),
+        )
+        .style(Style::new().white())
+        .highlight_style(Style::new().bold())
+        .highlight_symbol(">>")
+        .repeat_highlight_symbol(true)
+        .direction(ListDirection::TopToBottom);
 
         terminal
             .draw(|frame| frame.render_stateful_widget(list, frame.area(), &mut self.list_state))?;
@@ -81,6 +109,7 @@ impl App {
                 KeyCode::Up => self.list_state.select_previous(),
                 KeyCode::Enter => match self.list_state.selected().unwrap_or(2) {
                     0 => self.state = AppState::ListLocations,
+                    1 => self.state = AppState::ListItems,
                     2 => self.state = AppState::Exit,
                     _ => (),
                 },
