4474ce6e209b604557a208cf13da93aed83c67d2
Roosta078 <tommyagnello78@gmail.com>
2026-01-19 13:17:43 -0500

    before stack

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,11 @@ struct App {
 }
 
 pub trait Applet {
-    fn run(&mut self, terminal: &mut DefaultTerminal) -> Result<(), Box<dyn std::error::Error>> {
+    fn run(
+        &mut self,
+        terminal: &mut DefaultTerminal,
+        _db: &Inventory,
+    ) -> Result<(), Box<dyn std::error::Error>> {
         let text = Text::raw("TODO");
         terminal.draw(|frame| frame.render_widget(text, frame.area()))?;
         Ok(())
@@ -27,6 +31,7 @@ pub trait Applet {
 
 struct ListLocationApplet {
     exit_applet: bool,
+    table_state: TableState,
 }
 struct ListItemsApplet {
     exit_applet: bool,
@@ -34,7 +39,10 @@ struct ListItemsApplet {
 
 impl Default for ListLocationApplet {
     fn default() -> Self {
-        Self { exit_applet: false }
+        Self {
+            exit_applet: false,
+            table_state: TableState::default().with_selected_cell(Some((1, 0))),
+        }
     }
 }
 impl Default for ListItemsApplet {
@@ -44,15 +52,33 @@ impl Default for ListItemsApplet {
 }
 
 impl Applet for ListLocationApplet {
-    fn run(&mut self, terminal: &mut DefaultTerminal) -> Result<(), Box<dyn std::error::Error>> {
+    fn run(
+        &mut self,
+        terminal: &mut DefaultTerminal,
+        db: &Inventory,
+    ) -> Result<(), Box<dyn std::error::Error>> {
         self.exit_applet = false;
-        let mut table_state = TableState::default();
-        let rows = [
-            Row::new(vec!["Row11", "Row12", "Row13"]),
-            Row::new(vec!["Row21", "Row22", "Row23"]),
-            Row::new(vec!["Row31", "Row32", "Row33"]),
-        ];
 
+        let data = db.get_all_locations().unwrap_or_default();
+
+        let mut rows = vec![Row::new(vec!["Location ID", "Name", "Comment"])];
+
+        if data.is_empty() {
+            rows.push(Row::new(["DB ERROR", "DB ERROR", "DB_ERROR"]));
+        } else {
+            rows.append(
+                &mut data
+                    .iter()
+                    .map(|l| {
+                        Row::new([
+                            format!("{}", l.id),
+                            format!("{}", l.name),
+                            format!("{}", l.comment.clone().unwrap_or("".to_string())),
+                        ])
+                    })
+                    .collect::<Vec<Row>>(),
+            );
+        }
         // let widths = [
         //     Constraint::Length(5),
         //     Constraint::Length(10),
@@ -66,15 +92,21 @@ impl Applet for ListLocationApplet {
                     .title_bottom("Press 'q' or Esc to exit"),
             )
             .style(Style::new().white())
+            .cell_highlight_style(Style::new().red())
             .row_highlight_style(Style::new().reversed())
             .highlight_symbol(">>");
 
-        terminal
-            .draw(|frame| frame.render_stateful_widget(table, frame.area(), &mut table_state))?;
+        terminal.draw(|frame| {
+            frame.render_stateful_widget(table, frame.area(), &mut self.table_state)
+        })?;
 
         if let Some(key) = event::read()?.as_key_press_event() {
             match key.code {
                 KeyCode::Char('q') | KeyCode::Esc => self.exit_applet = true,
+                KeyCode::Down => self.table_state.select_next(),
+                KeyCode::Up => self.table_state.select_previous(),
+                KeyCode::Left => self.table_state.select_previous_column(),
+                KeyCode::Right => self.table_state.select_next_column(),
                 _ => {}
             }
         }
@@ -92,7 +124,11 @@ impl Applet for ListLocationApplet {
     }
 }
 impl Applet for ListItemsApplet {
-    fn run(&mut self, terminal: &mut DefaultTerminal) -> Result<(), Box<dyn std::error::Error>> {
+    fn run(
+        &mut self,
+        terminal: &mut DefaultTerminal,
+        db: &Inventory,
+    ) -> Result<(), Box<dyn std::error::Error>> {
         self.exit_applet = false;
         let text = Text::raw("TODO");
         terminal.draw(|frame| frame.render_widget(text, frame.area()))?;
@@ -149,11 +185,11 @@ impl App {
                 AppState::Exit => break,
                 AppState::TopMenu => self.run_top_menu(terminal)?,
                 AppState::ListLocations => {
-                    self.applets[0].run(terminal)?;
+                    self.applets[0].run(terminal, &self.db)?;
                     self.state = self.applets[0].get_next_state();
                 }
                 AppState::ListItems => {
-                    self.applets[1].run(terminal)?;
+                    self.applets[1].run(terminal, &self.db)?;
                     self.state = self.applets[1].get_next_state();
                 }
                 _ => break,
@@ -241,19 +277,15 @@ impl App {
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let l1 = db::inventory::Location {
-        id: 101,
-        name: "location1".to_string(),
-        comment: None,
-    };
-    let l2 = db::inventory::Location {
-        id: 102,
-        name: "location2".to_string(),
-        comment: Some("with comment".to_string()),
-    };
-
     let myapp = App::default();
-    myapp.db.add_location(&l1)?;
-    myapp.db.add_location(&l2)?;
+    for i in 0..100 {
+        let loc = db::inventory::Location {
+            id: i,
+            name: format!("location{i}").to_string(),
+            comment: Some(format!("comment{i}").to_string()),
+        };
+        myapp.db.add_location(&loc)?;
+    }
+
     ratatui::run(|terminal| myapp.run(terminal))
 }
