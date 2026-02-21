73477187430b7214c71a70fc1a02911e3fd20c17
Roosta078 <tommyagnello78@gmail.com>
2026-02-21 15:39:02 -0500

    item_lookup implemented an d tested

--- src/applets/item_lookup.rs
+++ src/applets/item_lookup.rs
@@ -1,11 +1,11 @@
 use super::applet::Applet;
+use crate::AppState;
 use crate::db::inventory;
-use crate::{App, AppState};
 use crossterm::event::{self, KeyCode};
 use ratatui::DefaultTerminal;
 use ratatui::layout::{Constraint, Layout, Position};
 use ratatui::style::Style;
-use ratatui::widgets::{Block, Padding, Paragraph};
+use ratatui::widgets::{Block, Paragraph};
 use std::error;
 use std::fmt;
 
@@ -44,13 +44,27 @@ impl ItemLookupApplet {
             id: String::default(),
         }
     }
+
+    fn find_item(&mut self, db: &inventory::Inventory) -> Result<(), Box<dyn std::error::Error>> {
+        let id = self
+            .id
+            .parse::<i64>()
+            .map_err(|_| ItemLookupError::new("Failed to parse Item ID"))?;
+
+        if !db.item_exists(id) {
+            return Err(ItemLookupError::new("Item ID does not exist"));
+        }
+
+        self.next_state = AppState::EditItem(id);
+        Ok(())
+    }
 }
 
 impl Applet for ItemLookupApplet {
     fn run(
         &mut self,
         terminal: &mut DefaultTerminal,
-        _db: &inventory::Inventory,
+        db: &inventory::Inventory,
     ) -> Result<(), Box<dyn std::error::Error>> {
         self.next_state = AppState::NoChange;
         let border = Block::bordered()
@@ -72,19 +86,42 @@ impl Applet for ItemLookupApplet {
         terminal.draw(|frame| {
             let inner_area = border.inner(frame.area());
             let [_, l1_area, id_area, _] = vertical.areas(inner_area);
+            let cent_id_area = id_area.centered_horizontally(Constraint::Length(20));
             frame.render_widget(border, frame.area());
             frame.render_widget(line1, l1_area);
-            frame.render_widget(
-                id_widget,
-                id_area.centered_horizontally(Constraint::Length(20)),
-            );
+            frame.render_widget(id_widget, cent_id_area);
+            frame.set_cursor_position(Position::new(
+                cent_id_area.x + self.cursor_position + 1,
+                cent_id_area.y + 1,
+            ));
         })?;
 
         if let Some(key) = event::read()?.as_key_press_event() {
             match key.code {
-                KeyCode::Enter | KeyCode::Esc | KeyCode::Char('q') => {
-                    self.next_state = AppState::Exit
+                KeyCode::Char(c) => {
+                    self.id.insert(self.cursor_position.into(), c);
+                    self.cursor_position += 1;
                 }
+                KeyCode::Esc => self.next_state = AppState::Exit,
+                KeyCode::Left => self.cursor_position = self.cursor_position.saturating_sub(1),
+                KeyCode::Right => {
+                    self.cursor_position = self
+                        .cursor_position
+                        .saturating_add(1)
+                        .min(self.id.len() as u16)
+                }
+                KeyCode::Backspace => {
+                    if self.cursor_position != 0 {
+                        self.cursor_position -= 1;
+                        self.id.remove(self.cursor_position.into());
+                    }
+                }
+                KeyCode::Delete => {
+                    if self.cursor_position != self.id.len() as u16 {
+                        self.id.remove(self.cursor_position.into());
+                    }
+                }
+                KeyCode::Enter => self.find_item(db)?,
                 _ => {}
             }
         }
@@ -104,5 +141,38 @@ mod item_lookup_tests {
         assert_eq!(my_applet.next_state, AppState::NoChange);
         assert_eq!(my_applet.cursor_position, 0);
         assert!(my_applet.id.is_empty());
+    }
+
+    #[test]
+    fn test_parsing() {
+        let my_inv = inventory::Inventory::open_in_memory().unwrap();
+        fill_db(&my_inv);
+
+        let mut my_applet = ItemLookupApplet::new();
+
+        my_applet.id = "text".into();
+        assert!(my_applet.find_item(&my_inv).is_err());
+
+        my_applet.id = "2".into();
+        assert!(my_applet.find_item(&my_inv).is_err());
+
+        my_applet.id = "".into();
+        assert!(my_applet.find_item(&my_inv).is_err());
+
+        my_applet.id = "101".into();
+        assert!(my_applet.find_item(&my_inv).is_ok());
+        assert_eq!(my_applet.next_state, AppState::EditItem(101));
+    }
+
+    fn fill_db(my_inv: &inventory::Inventory) {
+        for i in 0..5 {
+            let item = inventory::Item {
+                id: i + 100,
+                name: format!("item{i}").to_string(),
+                comment: Some(format!("comment{i}").to_string()),
+                location_id: None,
+            };
+            assert!(my_inv.add_item(&item).is_ok());
+        }
     }
 }
